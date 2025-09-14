669b96599ae3846724978be09e9a7b71cd3ec817
John Roux <john.roux1@gmail.com>
2025-09-14 23:05:58 +0200

    Updated User global search attribute to email

--- app/Filament/Admin/Resources/Users/UserResource.php
+++ app/Filament/Admin/Resources/Users/UserResource.php
@@ -22,7 +22,7 @@ class UserResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
-    protected static ?string $recordTitleAttribute = 'name';
+    protected static ?string $recordTitleAttribute = 'username';
     protected static ?string $pluralLabel = 'Users';
 
     public static function form(Schema $schema): Schema
